c654d29b908811eabaee61f0f76a63b7d96ba38b
Italo Rodrigues <italo.pa61@gmail.com>
2024-05-14 21:55:46 -0300

    crud

--- lanchonete/app/controllers/ingredientesses_controller.ts
+++ lanchonete/app/controllers/ingredientesses_controller.ts
@@ -1,9 +1,34 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
 
 import Ingredientess from "#models/ingredientess";
 
 export default class IngredientessesController {
-  async index(){
-    return await Ingredientess.all()
+  async index({request}: HttpContext){
+    const page = request.input('page',1)
+    const perPage = request.input('perPage',10)
+    return await Ingredientess.query().paginate(page,perPage)
+  }
+
+  async show({params}: HttpContext){
+    return await Ingredientess.findOrFail,(params.id)
+  }
+
+  async store({request}: HttpContext){
+    const dados = request.only(['nome','descricao'])
+    return await Ingredientess.create(dados)
+  }
+
+  async update({params, request}: HttpContext){
+    const ingredientess = await Ingredientess.findOrFail(params.id)
+    const dados = request.only(['nome','descricao'])
+    ingredientess.merge(dados)
+    return await ingredientess.save()
+  }
+
+  async destroy({params}: HttpContext){
+    const ingredientess = await Ingredientess.findOrFail(params.id)
+      await ingredientess.delete()
+      return {'ingredientess deletado:': ingredientess}
+
   }
 }
--- lanchonete/app/controllers/produtos_ingredientes_controller.ts
+++ lanchonete/app/controllers/produtos_ingredientes_controller.ts
@@ -1,9 +1,34 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
 
-import ProdutosIngrediente from "#models/produtos_ingrediente";
+import ProdutosIngrediente from '#models/produtos_ingrediente';
 
-export default class ProdutosIngredientesController {
-  async index(){
-    return await ProdutosIngrediente.all()
+export default class ProdutosIngredientesIngredientesController {
+  async index({request}: HttpContext){
+    const page = request.input('page',1)
+    const perPage = request.input('perPage',10)
+    return await ProdutosIngrediente.query().paginate(page,perPage)
+  }
+
+  async show({params}: HttpContext){
+    return await ProdutosIngrediente.findOrFail,(params.id)
+  }
+
+  async store({request}: HttpContext){
+    const dados = request.only(['produtoId','ingredienteId'])
+    return await ProdutosIngrediente.create(dados)
+  }
+
+  async update({params, request}: HttpContext){
+    const produtosingrediente = await ProdutosIngrediente.findOrFail(params.id)
+    const dados = request.only(['produtoId','ingredienteId'])
+    produtosingrediente.merge(dados)
+    return await produtosingrediente.save()
+  }
+
+  async destroy({params}: HttpContext){
+    const produtosingrediente = await ProdutosIngrediente.findOrFail(params.id)
+      await produtosingrediente.delete()
+      return {'produtosingrediente deletado:': produtosingrediente}
+
   }
 }
